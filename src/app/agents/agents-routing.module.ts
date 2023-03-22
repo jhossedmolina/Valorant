7a4eacc984a3042d5522d08f0de33feb6bdbbf67
Jhossed Pineda Molina <jhossedmolina@gmail.com>
2023-03-22 11:40:51 -0500

    Creating routes for Agents

--- src/app/agents/agents-routing.module.ts
+++ src/app/agents/agents-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgentsListComponent } from './pages/agents-list/agents-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AgentsListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AgentsRoutingModule { }
+export class AgentsRoutingModule {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'agents',
+    loadChildren: () =>
+      import('./agents/agents.module').then((m) => m.AgentsModule),
+  },
+  {
+    path: '',
+    redirectTo: 'agents',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

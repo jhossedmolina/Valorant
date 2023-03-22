6e6e4210af5f17488dd665b35f26f2d66f2a0cd3
Jhossed Pineda Molina <jhossedmolina@gmail.com>
2023-03-22 11:27:58 -0500

    Creating the Agent Module, Component, and Service

---
+++ src/app/agents/agents.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AgentsRoutingModule } from './agents-routing.module';
+import { AgentsListComponent } from './pages/agents-list/agents-list.component';
+
+
+@NgModule({
+  declarations: [
+    AgentsListComponent
+  ],
+  imports: [
+    CommonModule,
+    AgentsRoutingModule
+  ]
+})
+export class AgentsModule { }
---
+++ src/app/agents/pages/agents-list/agents-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agents-list',
+  templateUrl: './agents-list.component.html',
+  styleUrls: ['./agents-list.component.scss']
+})
+export class AgentsListComponent {
+
+}
